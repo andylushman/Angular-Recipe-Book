7623fbd8d29bebe2a9815203d89a8903a16a54fb
Andy Lushman <andylushman@gmail.com>
2018-06-01 10:19:47 -0600

    Created components

---
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+import { templateJitUrl } from "@angular/compiler";
+
+@Component({
+    selector: 'app-recipe-item',
+    templateUrl: './recipe-item.component.html',
+    styleUrls: ['./recipe-item.component.css']
+})
+
+export class RecipeItemComponent {
+
+}
